50bf0532f11cc2380b9030d50e057947b776a129
Daniela Mabel <118687158+Dan-Ar5@users.noreply.github.com>
2024-07-18 05:30:04 -0500

    Métodos Cancion.java

--- Cancion.java
+++ Cancion.java
@@ -19,4 +19,82 @@ public class Cancion {
     private double tempo;
     private double duration_ms;
     private int time_signature;
+
+
+        public String getArtistName() {
+        return artist_name;
+    }
+
+    public String getTrackName() {
+        return track_name;
+    }
+
+    public String getTrackId() {
+        return track_id;
+    }
+
+    public int getPopularity() {
+        return popularity;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getDanceability() {
+        return danceability;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public double getLoudness() {
+        return loudness;
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public double getSpeechiness() {
+        return speechiness;
+    }
+
+    public double getAcousticness() {
+        return acousticness;
+    }
+
+    public double getInstrumentalness() {
+        return instrumentalness;
+    }
+
+    public double getLiveness() {
+        return liveness;
+    }
+
+    public double getValence() {
+        return valence;
+    }
+
+    public double getTempo() {
+        return tempo;
+    }
+
+    public double getDurationMs() {
+        return duration_ms;
+    }
+
+    public int getTimeSignature() {
+        return time_signature;
+    }
+}
 }
