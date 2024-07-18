ea81957dec69af37c5f89648050221df2850e613
Ronald Pablo Garcia Valdivia <rgarciava@unsa.edu.pe>
2024-07-17 22:10:30 -0500

    Se creó la clase Cancion con los atributos necesarios del archivo que se nos dió

---
+++ Cancion.java
@@ -0,0 +1,22 @@
+public class Cancion {
+
+    private String artist_name;
+    private String track_name;
+    private String track__id;
+    private int popularity;
+    private int year;
+    private String genre;
+    private double danceability;
+    private double energy;
+    private int key;
+    private double loudness;
+    private int mode;
+    private double speechiness;
+    private double acousticness;
+    private double instrumentalness;
+    private double liveness;
+    private double valence;
+    private double tempo;
+    private double duration_ms;
+    private int time_signature;
+}
