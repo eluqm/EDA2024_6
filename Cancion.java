9a1b3f8bec2c0a0579d8c50142344d8a9609d819
kristennss <119991317+kristennss@users.noreply.github.com>
2024-07-23 19:00:15 -0500

    Cancion , métodos

--- Cancion.java
+++ Cancion.java
@@ -1,8 +1,7 @@
 public class Cancion {
-
     private String artist_name;
     private String track_name;
-    private String track__id;
+    private String track_id;
     private int popularity;
     private int year;
     private String genre;
@@ -20,8 +19,33 @@ public class Cancion {
     private double duration_ms;
     private int time_signature;
 
+    // Constructor y getters
 
-        public String getArtistName() {
+    public Cancion(String artist_name, String track_name, String track_id, int popularity, int year, String genre, double danceability, double energy, int key, double loudness, int mode, double speechiness, double acousticness, double instrumentalness, double liveness, double valence, double tempo, double duration_ms, int time_signature) {
+        this.artist_name = artist_name;
+        this.track_name = track_name;
+        this.track_id = track_id;
+        this.popularity = popularity;
+        this.year = year;
+        this.genre = genre;
+        this.danceability = danceability;
+        this.energy = energy;
+        this.key = key;
+        this.loudness = loudness;
+        this.mode = mode;
+        this.speechiness = speechiness;
+        this.acousticness = acousticness;
+        this.instrumentalness = instrumentalness;
+        this.liveness = liveness;
+        this.valence = valence;
+        this.tempo = tempo;
+        this.duration_ms = duration_ms;
+        this.time_signature = time_signature;
+    }
+
+    // Getters
+
+    public String getArtistName() {
         return artist_name;
     }
 
