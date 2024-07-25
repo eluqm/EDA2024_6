a7b0efc49000320e0d07ac8dda5c77a129f9a9a7
kristennss <119991317+kristennss@users.noreply.github.com>
2024-07-25 11:51:13 -0500

    Ordenar y Obtener

--- reproductor.py
+++ reproductor.py
@@ -5,3 +5,15 @@ class ReproductorMusica:
     def __init__(self):
         self.playlist = []
         self.canciones_disponibles = []
+
+
+
+
+    def ordenar_playlist(self, key, reverse=False):
+        self.playlist.sort(key=lambda cancion: getattr(cancion, key), reverse=reverse)
+
+    def obtener_canciones_disponibles(self):
+        return self.canciones_disponibles
+
+    def obtener_playlist(self):
+        return self.playlist
